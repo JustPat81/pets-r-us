fc6c45514c003d19bc25a017c162167ca2922a67
Patrick Wolff <pat.wolff81@gmail.com>
2022-06-19 08:52:34 -0500

    Adding comments to files

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+/*
+* ============================================
+* Title: index.js
+* Author: Patrick Wolff
+* Date: 19 June 2022
+* Modified By: Patrick Wolff
+* Description: index.js file to route pages for
+* the Pets-R-Us website.
+* ===========================================
+*/
+
 var express = require("express");
 var app = express()
 
+// Route to the pets-r-us landing page
 app.get("/index", function(request, response) {
     response.send("Welcome to the Pets-R-Us homepage!");
 });
 
+
+// Route to the grooming page
 app.get("/grooming", function(request, response) {
     response.send("Welcome to the Pets-R-Us grooming page.");
 });
 
+// Error if page requested in not found
 app.use(function(request, response) {
     response.status(404) .send("Page not found!")
 });
